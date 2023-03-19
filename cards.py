f40c8d00f9f29332c63ad89a981fa359dce3f238
Oleksandra Landau <landausa23@gmail.com>
2023-03-19 14:54:19 +0200

    start

---
+++ cards.py
@@ -0,0 +1,14 @@
+class Card:
+    def __init__(self, name, surname, company, position, email):
+        self.name = name
+        self.surname = surname
+        self.company = company
+        self.position = position
+        self.email = email
+
+
+person = Card(name="Kate", surname="Smith", company="Nestle", position="CEO", email="katesmith@gmail.com")
+person1 = Card(name="John", surname="Leon", company="Sony", position="SMM", email="johnleon@gmail.com")
+person2 = Card(name="Michael", surname="Jackson", company="Records", position="singer", email="mjackson2@gmail.com")
+person3 = Card(name="Sasha", surname="Landau", company="Friends Club", position="teacher", email="landausa@gmail.com")
+person4 = Card(name="Andrew", surname="Mohylev", company="Galaxy", position="manager", email="andrewfwf@gmail.com")
