5568a3c5359ddc8de55deca746e76a5768bf732a
Oleksandra Landau <landausa23@gmail.com>
2023-03-20 13:33:00 +0200

    two classes

--- cards.py
+++ cards.py
@@ -1,14 +1,30 @@
-class Card:
-    def __init__(self, name, surname, company, position, email):
+# main class
+class BaseContact:
+    def __init__(self, name, surname, pers_phone, email):
         self.name = name
         self.surname = surname
-        self.company = company
-        self.position = position
+        self.pers_phone = pers_phone
         self.email = email
 
 
-person = Card(name="Kate", surname="Smith", company="Nestle", position="CEO", email="katesmith@gmail.com")
-person1 = Card(name="John", surname="Leon", company="Sony", position="SMM", email="johnleon@gmail.com")
-person2 = Card(name="Michael", surname="Jackson", company="Records", position="singer", email="mjackson2@gmail.com")
-person3 = Card(name="Sasha", surname="Landau", company="Friends Club", position="teacher", email="landausa@gmail.com")
-person4 = Card(name="Andrew", surname="Mohylev", company="Galaxy", position="manager", email="andrewfwf@gmail.com")
+person = BaseContact(name="Kate", surname="Smith", pers_phone="+4888921422", email="katesmith@gmail.com")
+person1 = BaseContact(name="John", surname="Leon", pers_phone="+4819830033", email="johnleon@gmail.com")
+person2 = BaseContact(name="Michael", surname="Jackson", pers_phone="+4821910422", email="mckon2@gmail.com")
+person3 = BaseContact(name="Sasha", surname="Landau", pers_phone="+4813913100", email="landau@gmail.com")
+person4 = BaseContact(name="Andrew", surname="Joly", pers_phone="+4814920492", email="andrewfpf@gmail.com")
+
+
+# inheritance
+class BusinessContact(BaseContact):
+    def __init__(self, position, company, work_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.position = position
+        self.company = company
+        self.work_phone = work_phone
+
+
+per = BusinessContact(company="Nestle", position="CEO", work_phone="+8914828422")
+per1 = BusinessContact(company="Sony", position="SMM", work_phone="+313128422")
+per2 = BusinessContact(company="Records", position="singer", work_phone="+199320032")
+per3 = BusinessContact(company="Friends", position="teacher", work_phone="+523525390")
+per4 = BusinessContact(company="Galaxy", position="manager", work_phone="+9970768688")
