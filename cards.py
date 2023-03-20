ef6e85752e086eba3acb46f26c9575c4b847db99
Oleksandra Landau <landausa23@gmail.com>
2023-03-20 16:23:07 +0200

    3/4

--- cards.py
+++ cards.py
@@ -1,4 +1,4 @@
-# main class
+# first class
 class BaseContact:
     def __init__(self, name, surname, pers_phone, email):
         self.name = name
@@ -6,12 +6,18 @@ class BaseContact:
         self.pers_phone = pers_phone
         self.email = email
 
+    def contact(self):
+        print("I'm dialing", self.pers_phone, "and calling to", self.name, self.surname)
+
+    def label_length(self):
+        print(len(self.name) + len(self.surname))
+
 
 person = BaseContact(name="Kate", surname="Smith", pers_phone="+4888921422", email="katesmith@gmail.com")
-person1 = BaseContact(name="John", surname="Leon", pers_phone="+4819830033", email="johnleon@gmail.com")
-person2 = BaseContact(name="Michael", surname="Jackson", pers_phone="+4821910422", email="mckon2@gmail.com")
-person3 = BaseContact(name="Sasha", surname="Landau", pers_phone="+4813913100", email="landau@gmail.com")
-person4 = BaseContact(name="Andrew", surname="Joly", pers_phone="+4814920492", email="andrewfpf@gmail.com")
+person2 = BaseContact(name="John", surname="Leon", pers_phone="+4819830033", email="johnleon@gmail.com")
+person3 = BaseContact(name="Michael", surname="Jackson", pers_phone="+4821910422", email="mckon2@gmail.com")
+person4 = BaseContact(name="Sasha", surname="Landau", pers_phone="+4813913100", email="landau@gmail.com")
+person5 = BaseContact(name="Andrew", surname="Joly", pers_phone="+4814920492", email="andrewfpf@gmail.com")
 
 
 # inheritance
@@ -22,9 +28,27 @@ class BusinessContact(BaseContact):
         self.company = company
         self.work_phone = work_phone
 
+    def contact(self):
+        print("I'm dialing", self.work_phone, "and calling to", self.name, self.surname)
 
-per = BusinessContact(company="Nestle", position="CEO", work_phone="+8914828422")
-per1 = BusinessContact(company="Sony", position="SMM", work_phone="+313128422")
-per2 = BusinessContact(company="Records", position="singer", work_phone="+199320032")
-per3 = BusinessContact(company="Friends", position="teacher", work_phone="+523525390")
-per4 = BusinessContact(company="Galaxy", position="manager", work_phone="+9970768688")
+    def label_length(self):
+        print(len(self.name) + len(self.surname))
+
+
+b_contact = BusinessContact(name="Kate", surname="Smith", pers_phone="+4888921422", email="katesmith@gmail.com", company="Nestle", position="CEO", work_phone="+8914828422")
+b_contact2 = BusinessContact(name="John", surname="Leon", pers_phone="+4819830033", email="johnleon@gmail.com", company="Sony", position="SMM", work_phone="+3131289422")
+b_contact3 = BusinessContact(name="Michael", surname="Jackson", pers_phone="+4821910422", email="mckon2@gmail.com", company="Records", position="singer", work_phone="+1993200372")
+b_contact4 = BusinessContact(name="Sasha", surname="Landau", pers_phone="+4813913100", email="landau@gmail.com", company="Friends", position="teacher", work_phone="+5235225390")
+b_contact5 = BusinessContact(name="Andrew", surname="Joly", pers_phone="+4814920492", email="andrewfpf@gmail.com", company="Galaxy", position="manager", work_phone="+9970768688")
+
+
+BaseContact.contact(person2)
+BusinessContact.contact(b_contact2)
+BaseContact.label_length(person2)
+BusinessContact.label_length(b_contact3)
+
+from faker import Faker
+
+fake = Faker()
+
+print(fake.name())
